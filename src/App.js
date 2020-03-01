fc7606d04ae6d2c131823ac18a2a433ba7882b83
Mahmoud Hboubati <mhmoud.21830@gmail.com>
2020-03-01 18:59:00 +0400

    feat(design): question and answer design

--- src/App.js
+++ src/App.js
@@ -9,12 +9,24 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-      <button className="btn-amazing">
+
+      <div>
+        <button className="btn-amazing">
+          <img src={starImage} className="star" alt="logo" />
+          Do. Or do not. There is no try.
         <img src={starImage} className="star" alt="logo" />
-        Do. Or do not. There is no try.
-        <img src={starImage} className="star" alt="logo" />
-        <div className="shiny"></div>
-      </button>
+          <div className="shiny"></div>
+        </button>
+      </div>
+
+      <br />
+
+      <div class="questions-section">
+        <div class="question-answer">
+          <div class="question">What character (person) appeared in the most of StarWars films?</div>
+          <div id="mostAppearedAnswer" class="answer">Some answer</div>
+        </div>
+      </div>
     </div>
   );
 }
