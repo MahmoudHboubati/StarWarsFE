36c12b290d699029b2f7d0de4fc652b742762723
Mahmoud Hboubati <mhmoud.21830@gmail.com>
2020-03-04 20:34:58 +0400

    feat(*): move header to a separate component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import AmazingButtons from './components/AmazingButtons';
 import StarWarsInformation from './components/StarWarsInformation';
 import { Provider } from './store/context';
+import Header from './components/header/Header';
 
 function App() {
 
   return (
     <Provider>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-
+        <Header></Header>
         <AmazingButtons></AmazingButtons>
-
         <br />
-
         <StarWarsInformation></StarWarsInformation>
-
       </div>
     </Provider>
   );
