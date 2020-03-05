928a34a7abaafd27cc62fda83728c2b0b7f1021b
Mahmoud Hboubati <mhmoud.21830@gmail.com>
2020-03-06 02:01:56 +0400

    feat(test): change the global app test to chech amazing button presence

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders amazing button', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Do. Or do not. There is no try./i);
   expect(linkElement).toBeInTheDocument();
 });
